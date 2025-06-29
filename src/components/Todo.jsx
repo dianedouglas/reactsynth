5a9f7642d028b50d242fc578d9f88a030023d120
Diane Douglas <dianedouglas@dianes-air.mynetworksettings.com>
2025-06-29 17:05:55 -0400

    Alternate way of updating completed tasks.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,24 @@
+import { useState } from 'react'
 import { FiDelete } from "react-icons/fi";
 import { update_todo } from '../api/endpoints'
 
 
 export function Todo({id, title, completed, deleteTodo}){
 
-
+	const [isChecked, setChecked] = useState(completed)
 
 	const handleDelete = async () => {
 		await deleteTodo(id);
 	}
 
 	const handleComplete = async (e) => {
-		await update_todo(id, e.target.checked);
+		await update_todo(id, !isChecked);
+		setChecked(!isChecked);
 	}
 
 	return (
 		<div className="todo-container">
-			<input onChange={(e) => handleComplete(e)} type="checkbox" defaultChecked={completed}/>
+			<input onClick={handleComplete} type="checkbox" defaultChecked={isChecked}/>
 			<h3 className="todo-title">{title}</h3>
 			<FiDelete onClick={handleDelete} className="delete" size='20px'/>
 		</div>
