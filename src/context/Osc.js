f32d34855582c474dea7f5162fcb7603f59cad7e
Diane Douglas <diane.douglas1@gmail.com>
2025-07-02 17:34:14 -0400

    Fix click when disconnecting osc too early.

--- src/context/Osc.js
+++ src/context/Osc.js
@@ -4,7 +4,7 @@ export default class Osc {
     this.envelope = envelope || {
       attack: 0.1,
       decay: 0.1,
-      sustain: 0.618,
+      sustain: 0.6,
       release: 1,
     };
     this.osc = actx.createOscillator();
@@ -15,38 +15,22 @@ export default class Osc {
     this.gateGain.gain.value = 0;
     this.osc.connect(this.gateGain);
     this.gateGain.connect(connection);
-    this.easing = 0.005;
+    this.easing = 0.1;
     this.osc.start();
     this.start();
   }
   start(){
     let {currentTime} = this.actx;
     this.gateGain.gain.cancelScheduledValues(currentTime);
-    this.gateGain.gain.setValueAtTime(0, 
-      currentTime + this.easing);
-    // dont need decay we are making drones. just sustain level.
-    // this.gateGain.gain.linearRampToValueAtTime(1, 
-    //   currentTime + this.envelope.attack + this.easing);
-    this.gateGain.gain.linearRampToValueAtTime(this.envelope.sustain, 
-      currentTime + this.envelope.attack + this.envelope.decay + this.easing);
-
-    // temporarily hardcoding sustain 
-    this.gateGain.gain.linearRampToValueAtTime(0, 
-      currentTime + this.envelope.attack + this.envelope.decay + this.envelope.release + this.easing);
+    let startEnvelopeTime = currentTime + this.easing;
+    this.gateGain.gain.setValueAtTime(0, startEnvelopeTime);
+    let attackToSustainLevelTime = startEnvelopeTime + this.envelope.attack + this.easing;
+    this.gateGain.gain.linearRampToValueAtTime(this.envelope.sustain, attackToSustainLevelTime);
+    let releaseTo0Time = attackToSustainLevelTime + this.envelope.release + this.easing;
+    this.gateGain.gain.linearRampToValueAtTime(0, releaseTo0Time);
+    let disconnectTime = releaseTo0Time + this.easing;
     setTimeout(()=>{    
       this.osc.disconnect();
-    }, 1100);
+    }, 2000);
   }
-  // stop(){
-  //   let {currentTime} = this.actx;
-  //   this.gateGain.gain.cancelScheduledValues(currentTime);
-  //   this.gateGain.gain.setTargetAtTime(
-  //     0,
-  //     currentTime,
-  //     this.envelope.release + this.easing
-  //   )
-  //   setTimeout(()=>{    
-  //     this.osc.disconnect();
-  //   }, 10000);
-  // }
 }
